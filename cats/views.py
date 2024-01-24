4e9c090feae41f0a631dabf3e6bf641bd85f259e
Anastasia Barshina <barshina.av@gmail.com>
2024-01-24 23:45:32 +0300

    Add filters, pagination. Update permissions

--- cats/views.py
+++ cats/views.py
@@ -1,16 +1,89 @@
-from rest_framework import viewsets
+from rest_framework import viewsets  # permissions
+# from rest_framework.throttling import AnonRateThrottle
+from rest_framework.throttling import ScopedRateThrottle
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 from .models import Achievement, Cat, User
-
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
+from .permissions import OwnerOrReadOnly, ReadOnly
+from .throttling import WorkingHoursRateThrottle
+from .pagination import CatsPagination
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    # Устанавливаем разрешение
+    # permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (OwnerOrReadOnly,)
+    # throttle_classes = (AnonRateThrottle,)  # Подключили класс AnonRateThrottle
+
+    # Если кастомный тротлинг-класс вернёт True - запросы будут обработаны
+    # Если он вернёт False - все запросы будут отклонены
+    throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
+    # А далее применится лимит low_request
+
+    # Для любых пользователей установим кастомный лимит 1 запрос в минуту
+    throttle_scope = 'low_request'
+
+    # pagination_class = PageNumberPagination
+
+    # Даже если на уровне проекта установлен PageNumberPagination
+    # Для котиков будет работать LimitOffsetPagination
+    # pagination_class = LimitOffsetPagination
+
+    # Вот он наш собственный класс пагинации с page_size=20
+    # pagination_class = CatsPagination
+
+    # Указываем фильтрующий бэкенд DjangoFilterBackend
+    # Из библиотеки django-filter
+    # filter_backends = (DjangoFilterBackend,)
+    # Временно отключим пагинацию на уровне вьюсета,
+    # так будет удобнее настраивать фильтрацию
+    pagination_class = None
+    # Фильтровать будем по полям color и birth_year модели Cat
+    filterset_fields = ('color', 'birth_year')
+
+    # Добавим в кортеж ещё один бэкенд
+    # filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    # search_fields = ('name',)
+
+    # filter_backends = (filters.SearchFilter,)
+    # search_fields = ('achievements__name', 'owner__username')
+
+    # Если имя котика должно начинаться с указанной в параметре search строки
+    # Определим, что значение параметра search должно быть началом искомой строки
+    # search_fields = ('^name',)
+
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,
+                       filters.OrderingFilter)
+    search_fields = ('name',)
+    ordering_fields = ('name', 'birth_year')
+    # Порядок выдачи котиков по умолчанию
+    ordering = ('birth_year',)
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+        serializer.save(owner=self.request.user)
+
+    def get_permissions(self):
+        # Если в GET-запросе требуется получить информацию об объекте
+        if self.action == 'retrieve':
+            # Вернём обновлённый перечень используемых пермишенов
+            return (ReadOnly(),)
+        # Для остальных ситуаций оставим текущий перечень пермишенов без изменений
+        return super().get_permissions()
+
+    def get_queryset(self):
+        queryset = Cat.objects.all()
+        # Добыть параметр color из GET-запроса
+        color = self.request.query_params.get('color')
+        if color is not None:
+            #  через ORM отфильтровать объекты модели Cat
+            #  по значению параметра color, полученного в запросе
+            queryset = queryset.filter(color=color)
+        return queryset
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
